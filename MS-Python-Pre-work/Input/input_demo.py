701865aa6e39a38cd67e125622fb0d9526329c9f
dennisnyamweya <denisnyamweya813@gmail.com>
2019-07-15 17:09:39 +0300

    using range

--- MS-Python-Pre-work/Input/input_demo.py
+++ MS-Python-Pre-work/Input/input_demo.py
@@ -12,3 +12,10 @@ print(age) #Prints name and age if initiated with both name and age
 
 list_a = list(range(0,5))#Comment out to run and vice versa
 print(list_a)#Displays list from 0 to 5
+#
+#
+# ##########################Using range#################################
+
+
+for i in range(0,7):
+    print("I would love " + str(i) + " cookies")
