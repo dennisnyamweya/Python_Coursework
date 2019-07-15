3c2e43e238d30147bb76c6e7ae9e4d58f5f1d9df
dennisnyamweya <denisnyamweya813@gmail.com>
2019-07-15 17:02:59 +0300

    lms

--- MS-Python-Pre-work/Input/input_demo.py
+++ MS-Python-Pre-work/Input/input_demo.py
@@ -3,8 +3,12 @@ import sys
 name = sys.argv[1]
 age = sys.argv[2]
 
-# print("How old are you?")
+# print("How old are you?"))#Comment out to run and vice versa
 # age = int(input())
 
 print(name)
-print(age)
+print(age) #Prints name and age if initiated with both name and age
+
+
+list_a = list(range(0,5))#Comment out to run and vice versa
+print(list_a)#Displays list from 0 to 5
