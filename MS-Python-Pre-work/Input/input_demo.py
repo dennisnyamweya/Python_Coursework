86b2de1a3d6bc712ea9e71c4cfe97b8287020848
dennisnyamweya <denisnyamweya813@gmail.com>
2019-07-15 16:42:05 +0300

    python assignments

---
+++ MS-Python-Pre-work/Input/input_demo.py
@@ -0,0 +1,10 @@
+import sys
+
+name = sys.argv[1]
+age = sys.argv[2]
+
+# print("How old are you?")
+# age = int(input())
+
+print(name)
+print(age)
---
+++ MS-Python-Pre-work/example.py
@@ -0,0 +1,16 @@
+import random
+
+# We can generate a random number using the randint method
+
+randomnumber = random.randint(0,10)
+
+print(randomnumber)
+#######################Circumference Calculation Random Number######################
+# start="Circumference Calculator of Random Radius from 10 to 20"
+#
+# print(start)
+#
+# π=3.14
+# random_radius=float()
+# random_radius=random_radius.randint(1,10)
+# print("The Circumference is:",round(2*π*radius))
