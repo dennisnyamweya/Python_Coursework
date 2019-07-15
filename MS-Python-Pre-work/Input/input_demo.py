7398b7d58bd971ae1f27a4a9b46af63fcc03a512
dennisnyamweya <denisnyamweya813@gmail.com>
2019-07-15 17:20:16 +0300

    checking availabe numbers and printing

--- MS-Python-Pre-work/Input/input_demo.py
+++ MS-Python-Pre-work/Input/input_demo.py
@@ -22,7 +22,25 @@
 
 ###########################For modulus loop##############################
 
-numbers = [1, 2, 3, 4, 5]
-for i in numbers:  #Checks out if it is divisible by 2
-    if i % 2 == 0:
-        print(i)  #Prints out numbers divisible by 2 i.e 2 and 4
+# numbers = [1, 2, 3, 4, 5]
+# for i in numbers:  #Checks out if it is divisible by 2
+#     if i % 2 == 0:
+#         print(i)  #Prints out numbers divisible by 2 i.e 2 and 4
+########################### Printing in range############################
+
+'''
+in a team of members 20, some numbers are taken
+and want to display the numbers that are not taken
+so others don't pick the picked numbers
+'''
+
+# taken numbers
+numTaken = [3,5,7,11,13] #Are numbers taken
+
+print("Available numbers")#Directive to print avaliable numbers
+
+# loop
+for i in range(1,21):#range defines it from 1 to 21
+    if i in numTaken:#Condition confirmation
+        continue
+    print(i)#Directive to print the numbers
