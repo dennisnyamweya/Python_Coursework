76af6d672a7b782574f3b1a471d1950b80a7ff5f
dennisnyamweya <denisnyamweya813@gmail.com>
2019-07-16 11:19:27 +0300

    all project mad-libs

--- MS-Python-Pre-work/Mini Projects /bmi_calculator.py
+++ MS-Python-Pre-work/Mini Projects /bmi_calculator.py
@@ -6,4 +6,4 @@ print(start)
 
 height = float(input("Input your height in meters: "))  #Takes input of height in meters
 weight = float(input("Input your weight in kilogram: "))  #Takes input of weight in kg
-print("Your body mass index is: ", round(weight / (height * height), 2))  #Finds out the BDD
+print("Your body mass index is: ", round(weight / (height**2)))  #Finds out the BDD
